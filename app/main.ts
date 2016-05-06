ed0bf432721841b3b4285f81a97e154a8c54c5e1
Maxime <maxime.shubin@gmail.com>
2016-05-06 18:01:13 +0200

    Inject services into the app

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,11 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ConfigurationService } from './configuration.service';
+import { MizarService } from './mizar.service';
+import { LayerManagerService } from './layerManager.service';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+	HTTP_PROVIDERS, ConfigurationService, MizarService, LayerManagerService
+]);
