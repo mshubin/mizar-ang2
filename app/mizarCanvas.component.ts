45d6e3919cb5914471abf5b802117db7cd9b3a85
Maxime <maxime.shubin@gmail.com>
2016-05-16 13:45:58 +0200

    Add tiny spinner on first load of background layer

--- app/mizarCanvas.component.ts
+++ app/mizarCanvas.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 
 import { MizarService } from './mizar.service';
+import { MdSpinner, MdProgressCircle } from '@angular2-material/progress-circle';
 
 declare var AstroWeb: any;
 
 @Component({
 	selector: 'mizar-canvas',
+	styleUrls: [
+		'../node_modules/@angular2-material/progress-circle/progress-circle.css'
+	],
 	template: `
 		<div class="canvas">
+			<div style="position: absolute; top: 50%; left: 50%;" *ngIf="isLoading">
+				<md-progress-circle style="width: 30px;" mode="indeterminate" color="primary"></md-progress-circle>
+			</div>
 			<canvas class="noSelect" id="{{id}}" style="border: none; margin: 0; padding: 0;" width="0" height="0"></canvas>
 		</div>
-	`
+	`,
+	directives: [MdProgressCircle]
 })
 export class MizarCanvas {
 
+	isLoading: boolean = true;
 	id:string;
+
 	constructor(private _mizarService:MizarService){
 		this.id = "AstroWebCanvas";
 		//this.conf = configrationService.getConfiguration();
@@ -61,6 +71,11 @@ export class MizarCanvas {
 		this._mizarService.init(this.id);
 		this.initGlobeEvents();
 		this.initCanvas(document.getElementById(this.id));
+
+		this._mizarService.globe.subscribe("baseLayersReady", () => {
+			this.isLoading = false;
+		});
+		
 		//if ( options.isMobile )
 		//{	
 			// TODO
